4b4c8da35dbcdf08b222a18c693acac56f37f7ee
Glowrot <ICEIE99999@msn.com>
2026-03-03 02:01:55 -0800

    Add files via upload

---
+++ GuildRecipes.lua
@@ -0,0 +1,267 @@
+---@class GuildRecipes
+GuildRecipes = GuildRecipes or {}
+
+---@class GuildRecipes
+local m = GuildRecipes
+
+---@diagnostic disable-next-line: undefined-global
+local lib_stub = LibStub
+
+GuildRecipes.name = "GuildRecipes"
+GuildRecipes.prefix = "GRECIPES"
+GuildRecipes.tagcolor = "FFDD7744"
+GuildRecipes.events = {}
+GuildRecipes.debug_enabled = false
+
+BINDING_HEADER_GUILDRECIPES = "GuildRecipes"
+
+---@class Item
+---@field id integer
+---@field name string
+---@field quality integer?
+---@field icon string?
+
+---@alias NotAceTimer any
+---@alias TimerId number
+
+---@class AceTimer
+---@field ScheduleTimer fun( self: NotAceTimer, callback: function, delay: number, ... ): TimerId
+---@field ScheduleRepeatingTimer fun( self: NotAceTimer, callback: function, delay: number, arg: any ): TimerId
+---@field CancelTimer fun( self: NotAceTimer, timer_id: number )
+---@field TimeLeft fun( self: NotAceTimer, timer_id: number )
+
+---@class AceSerializer
+---@field Serialize fun( self: any, ... ): string
+---@field Deserialize fun( self: any, str: string ): any
+
+---@class AceComm
+---@field RegisterComm fun( self: any, prefix: string, method: function? )
+---@field SendCommMessage fun( self: any, prefix: string, text: string, distribution: string, target: string?, prio: "BULK"|"NORMAL"|"ALERT"?, callbackFn: function?, callbackArg: any? )
+
+function GuildRecipes:init()
+	self.frame = CreateFrame( "Frame" )
+	self.frame:SetScript( "OnEvent", function()
+		if m.events[ event ] then
+			m.events[ event ]()
+		end
+	end )
+
+	for k, _ in pairs( m.events ) do
+		m.frame:RegisterEvent( k )
+	end
+end
+
+function GuildRecipes.events.ADDON_LOADED()
+	if arg1 == m.name then
+		---@type AceTimer
+		m.ace_timer = lib_stub( "AceTimer-3.0" )
+
+		---@type AceSerializer
+		m.ace_serializer = lib_stub( "AceSerializer-3.0" )
+
+		---@type AceComm
+		m.ace_comm = lib_stub( "AceComm-3.0" )
+
+		---@type MessageHandler
+		m.msg = m.MessageHandler.new( m.ace_timer, m.ace_serializer, m.ace_comm )
+
+		---@type TradeskillGui
+		m.tsgui = m.Tradeskills.new()
+
+		---@type SlashCommand
+		m.slash_command = m.SlashCommand.new( m.name, { "gr", "GuildRecipes" } )
+
+		m.version = GetAddOnMetadata( m.name, "Version" )
+		m.info( string.format( "(v%s) Loaded", m.version ) )
+	end
+end
+
+function GuildRecipes.events.PLAYER_LOGIN()
+	-- Initialize DB
+	GuildRecipesDB = GuildRecipesDB or {}
+	m.db = GuildRecipesDB
+	m.db.players = m.db.players or {}
+	m.db.tradeskills = m.db.tradeskills or {}
+	m.db.tradeskills_last_update = m.db.tradeskills_last_update or {}
+	m.db.frame_tradeskills = m.db.frame_tradeskills or {}
+
+	m.player = UnitName( "player" )
+	m.player_class = UnitClass( "player" )
+	m.slash_command.init()
+
+	m.tooltip = CreateFrame( "GameTooltip", "GuildRecipesTooltip", nil, "GameTooltipTemplate" )
+	m.tooltip:SetOwner( WorldFrame, "ANCHOR_NONE" )
+
+	m.update_data()
+end
+
+function GuildRecipes.events.TRADE_SKILL_SHOW()
+	local reverse = m.build_reverse_trade_map( GetLocale() )
+	local tradeskill = reverse[ GetTradeSkillLine() ]
+
+	if m.TRADE_SKILL_LOCALIZATION[ tradeskill ] then
+		local num = GetNumTradeSkills()
+
+		for i = 1, GetNumTradeSkills() do
+			local _, type = GetTradeSkillInfo( i )
+			if type == "header" then
+				num = num - 1
+			end
+		end
+
+		m.db.tradeskills[ tradeskill ] = m.db.tradeskills[ tradeskill ] or {}
+
+		if m.count_recipes( m.db.tradeskills[ tradeskill ], m.player ) ~= num then
+			for i = 1, GetNumTradeSkills() do
+				local _, type = GetTradeSkillInfo( i )
+				if type ~= "header" then
+					local id, name, quality = m.parse_item_link( GetTradeSkillItemLink( i ) )
+					local item = {
+						id = id,
+						name = name,
+						quality = quality
+					}
+					m.update_tradeskill_item( tradeskill, item, { m.player } )
+				end
+			end
+
+			m.db.tradeskills_last_update[ tradeskill ] = m.get_server_timestamp()
+			m.msg.send_tradeskill( tradeskill )
+		end
+	end
+end
+
+function GuildRecipes.events.CRAFT_SHOW()
+	local reverse = m.build_reverse_trade_map( GetLocale() )
+	local tradeskill = reverse[ GetCraftName() ]
+
+	if tradeskill == "Enchanting" then
+		local num = GetNumCrafts()
+
+		m.db.tradeskills[ tradeskill ] = m.db.tradeskills[ tradeskill ] or {}
+
+		if m.count_recipes( m.db.tradeskills[ tradeskill ], m.player ) ~= num then
+			for i = 1, num do
+				local id, name, quality = m.parse_item_link( GetCraftItemLink( i ) )
+				local item = {
+					id = id,
+					name = name,
+					quality = quality
+				}
+				m.update_tradeskill_item( tradeskill, item, { m.player } )
+			end
+
+			m.db.tradeskills_last_update[ tradeskill ] = m.get_server_timestamp()
+			m.msg.send_tradeskill( tradeskill )
+		end
+	end
+end
+
+function GuildRecipes.events.UNIT_INVENTORY_CHANGED()
+	if arg1 == "player" and m.tsgui.is_visible() then
+		m.tsgui.update()
+	end
+end
+
+---@param tradeskill string
+---@param item Item
+---@param players string[]
+function GuildRecipes.update_tradeskill_item( tradeskill, item, players )
+	if item.id then
+		local player_ids = {}
+		for _, p in pairs( players ) do
+			if not m.find( p, m.db.players ) then
+				table.insert( m.db.players, p )
+			end
+
+			local _, player_id = m.find( p, m.db.players )
+			if player_id then
+				table.insert( player_ids, player_id )
+			end
+		end
+
+		if m.db.tradeskills[ tradeskill ][ item.id ] then
+			m.debug( string.format( "Updating %s: %s", tradeskill, item.name ) )
+			if not players then
+				m.debug( "ERROR, no players for: " .. tostring( item.name ) )
+				return
+			end
+
+			local recipe_players = m.comma_separated_to_table( m.db.tradeskills[ tradeskill ][ item.id ].p )
+			for _, p in pairs( players ) do
+				local _, player_id = m.find( p, m.db.players )
+
+				if player_id and not m.find( tostring(player_id), recipe_players ) then
+					table.insert( recipe_players, player_id )
+				end
+			end
+			m.db.tradeskills[ tradeskill ][ item.id ].p = m.table_to_comma_separated( recipe_players )
+		else
+			m.debug( string.format( "Adding %s: %s", tradeskill, item.name ) )
+
+			m.db.tradeskills[ tradeskill ][ item.id ] = {
+				n = item.name,
+				q = item.quality,
+				p = m.table_to_comma_separated( player_ids )
+			}
+		end
+	end
+end
+
+function GuildRecipes.update_data()
+	-- If we haven't updated in the last 48 hours, request an update
+	if next(m.db.tradeskills_last_update) == nil then
+		m.msg.request_tradeskills()
+	else
+		local now = m.get_server_timestamp()
+		for tradeskill, last_update in pairs( m.db.tradeskills_last_update ) do
+			if now >= last_update + 172800 then
+				m.msg.request_tradeskill( tradeskill )
+				break
+			end
+		end
+	end
+end
+
+---@param bag_start integer
+---@param bag_end integer
+---@param name string
+---@return integer
+function GuildRecipes.find_item_count_bag( bag_start, bag_end, name )
+	local count = 0
+	for bag = bag_start, bag_end do
+		local slots = GetContainerNumSlots( bag )
+		for slot = 1, slots do
+			local _, item_count = GetContainerItemInfo( bag, slot )
+			if item_count and item_count > 0 then
+				local _, item_name = m.parse_item_link( GetContainerItemLink( bag, slot ) )
+				if item_name == name then
+					count = count + item_count
+				end
+			end
+		end
+	end
+	return count
+end
+
+function GuildRecipes.fix()
+	for tradeskill, recipes in pairs( m.db.tradeskills ) do
+		for id, recipe in pairs( recipes ) do
+			if recipe.p and type( recipe.p ) == "table" then
+				local players = ""
+				for _, p in pairs( recipe.p ) do
+					if players ~= "" then
+						players = players .. ","
+					end
+					players = players .. p
+				end
+				--	local _, pi = m.find( p, m.db.players)
+				--	table.insert( players, pi )
+
+				recipe.p = players
+			end
+		end
+	end
+end
+
+GuildRecipes:init()
---
+++ MessageHandler.lua
@@ -0,0 +1,296 @@
+GuildRecipes = GuildRecipes or {}
+
+---@class GuildRecipes
+local m = GuildRecipes
+
+if m.MessageHandler then return end
+
+---@type MessageCommand
+local MessageCommand = {
+	RequestTradeskill = "RTS",
+	Tradeskill = "TS",
+	Ping = "PING",
+	Pong = "PONG",
+	VersionCheck = "VERSIONCHECK",
+	Version = "VERSION",
+}
+
+---@alias MessageCommand
+---| "RTS"
+---| "TS"
+---| "PING"
+---| "PONG"
+---| "VERSIONCHECK"
+---| "VERSION"
+
+---@class MessageHandler
+---@field send_tradeskill fun( tradeskill: string )
+---@field request_tradeskill fun( tradeskill: string )
+---@field request_tradeskills fun()
+---@field version_check fun()
+
+local M = {}
+
+---@param ace_timer AceTimer
+---@param ace_serializer AceSerializer
+---@param ace_comm AceComm
+function M.new( ace_timer, ace_serializer, ace_comm )
+	local pinging = false
+	local best_ping = {}
+	local var_names = {
+		n = "name",
+		q = "quality",
+		c = "count",
+		p = "players",
+		d = "data",
+		i = "items",
+		ts = "tradeskill",
+		h = "hashes",
+		lu = "last_update",
+		--tlu = "tradeskills_last_update",
+		Alc = "Alchemy",
+		Bla = "Blacksmithing",
+		Eng = "Engineering",
+		Enc = "Enchanting",
+		Lea = "Leatherworking",
+		Tai = "Tailoring",
+		Jew = "Jewelcrafting",
+
+	}
+	setmetatable( var_names, { __index = function( _, key ) return key end } );
+
+	---@param t table
+	local function decode( t )
+		local l = {}
+		for key, value in pairs( t ) do
+			if type( value ) == "table" then
+				value = decode( value )
+			end
+
+			l[ var_names[ key ] ] = value
+		end
+		return l
+	end
+
+	---@param command MessageCommand
+	---@param data table?
+	local function broadcast( command, data )
+		m.debug( string.format( "Broadcasting %s", command ) )
+
+		ace_comm:SendCommMessage( m.prefix, command .. "::" .. ace_serializer.Serialize( M, data ), "GUILD", nil, "NORMAL" )
+	end
+
+	local function send_tradeskill( tradeskill )
+		m.debug( string.format( "Sending %s", tradeskill ) )
+		local data = {
+			tradeskill = tradeskill,
+			recipes = {}
+		}
+
+		for id, item in m.db.tradeskills[ tradeskill ] do
+			local players = {}
+			for _, p in pairs( m.comma_separated_to_table( item.p ) ) do
+				table.insert( players, m.db.players[ tonumber( p ) ] )
+			end
+
+			table.insert( data.recipes, {
+				id = id,
+				p = players
+			} )
+		end
+
+		broadcast( MessageCommand.Tradeskill, data )
+	end
+
+	local function send_tradeskills()
+		for tradeskill in m.db.tradeskills do
+			send_tradeskill( tradeskill )
+		end
+	end
+
+	local function request_tradeskill( tradeskill )
+		pinging = true
+		best_ping = {}
+
+		broadcast( MessageCommand.Ping, {
+			[string.sub( tradeskill, 1, 3 )] = m.tradeskill_hash( tradeskill )
+		} )
+	end
+
+	local function request_tradeskills()
+		pinging = true
+		best_ping = {}
+		local hashes = {}
+
+		for tradeskill in pairs(m.TRADE_SKILL_LOCALIZATION) do
+			hashes[ string.sub( tradeskill, 1, 3 ) ] = m.tradeskill_hash( tradeskill )
+		end
+
+		broadcast( MessageCommand.Ping, hashes )
+	end
+
+	local function version_check()
+		broadcast( MessageCommand.VersionCheck )
+	end
+
+	---@param command string
+	---@param data table
+	---@param sender string
+	local function on_command( command, data, sender )
+		if command == MessageCommand.Tradeskill then
+			--
+			-- Receive tradeskill
+			--
+			m.debug( string.format( "Receiving %s from %s.", data.tradeskill, sender ) )
+			local tradeskill = data.tradeskill
+			m.db.tradeskills[ tradeskill ] = m.db.tradeskills[ tradeskill ] or {}
+
+			for _, v in data.recipes do
+				if v.id then
+					---@type Item
+					local item = nil
+					if m.db.tradeskills[ tradeskill ][ v.id ] then
+						item = {
+							id = v.id,
+							name = m.db.tradeskills[ tradeskill ][ v.id ].n,
+							quality = m.db.tradeskills[ tradeskill ][ v.id ].q
+						}
+					else
+						if tradeskill == "Enchanting" then
+							if v and v.id then
+								local name = m.Enchants[ v.id ] and m.Enchants[ v.id ].name
+								if not name then
+									m.error( string.format( "Unknown enchantment received (%d)", v.id ) )
+								else
+									item = {
+										id = v.id,
+										name = name,
+									}
+								end
+							else
+								m.debug( "empty enchant data??" )
+							end
+						else
+							m.debug( "Fetching item info for " .. tostring( v.id ) )
+							m.get_item_info( v.id, function( item_info, players )
+								if item_info then
+									local i = {
+										id = item_info.id,
+										name = item_info.name,
+										quality = item_info.quality
+									}
+
+									m.update_tradeskill_item( tradeskill, i, players )
+								else
+									m.debug( "No item_info for " .. tostring( v.id ) )
+								end
+							end, v.players )
+						end
+					end
+
+					if item then
+						--m.debug( "got item" )
+						m.update_tradeskill_item( tradeskill, item, v.players )
+					end
+				end
+			end
+
+			m.db.tradeskills_last_update[ tradeskill ] = m.get_server_timestamp()
+		elseif command == MessageCommand.RequestTradeskill and data.player == m.player then
+			--
+			-- Request for tradeskill
+			--
+			m.debug(m.dump(data))
+			send_tradeskill( data.tradeskill )
+		elseif command == MessageCommand.Ping and sender ~= m.player then
+			--
+			-- Recive ping
+			--
+			for tradeskill, hash in (data or {}) do
+				local local_hash = m.tradeskill_hash( tradeskill )
+				if local_hash == hash then
+					data[ tradeskill ] = nil
+				else
+					data[ tradeskill ] = m.db.tradeskills_last_update[ tradeskill ] or 0
+				end
+			end
+
+			m.debug( m.dump( data ) )
+			broadcast( MessageCommand.Pong, data )
+		elseif command == MessageCommand.Pong and pinging then
+			--
+			-- Receive pong
+			--
+			m.debug( m.dump( data ) )
+			for tradeskill, last_update in pairs( data or {} ) do
+				if not best_ping[ tradeskill ] or (last_update > best_ping[ tradeskill ].last_update) then
+					best_ping[ tradeskill ] = {
+						player = sender,
+						last_update = last_update
+					}
+				end
+			end
+
+			if ace_timer:TimeLeft( M[ "ping_timer" ] ) == 0 then
+				M[ "ping_timer" ] = ace_timer.ScheduleTimer( M, function()
+					if pinging then
+						m.debug( "Ping timeout, requesting tradeskills from best pings." )
+						pinging = false
+						for tradeskill, ping_info in pairs( best_ping ) do
+							m.debug( string.format( "Requesting %s from %s (last update: %d)", tradeskill, ping_info.player, ping_info.last_update ) )
+							broadcast( MessageCommand.RequestTradeskill, {
+								player = ping_info.player,
+								ts = tradeskill
+							} )
+						end
+					end
+				end, 2 )
+			end
+		elseif command == MessageCommand.VersionCheck then
+			--
+			-- Receive version request
+			--
+			broadcast( MessageCommand.Version, { requester = sender, version = m.version, class = m.player_class } )
+		elseif command == MessageCommand.Version then
+			--
+			-- Receive version
+			--
+			if data.requester == m.player then
+				m.info( string.format( "%s [v%s]", m.colorize_player_by_class( sender, data.class ), data.version ), true )
+			end
+		end
+	end
+
+	local function on_comm_received( prefix, data_str, _, sender )
+		if prefix ~= m.prefix or sender == m.player then return end
+
+		local command = string.match( data_str, "^(.-)::" )
+		data_str = string.gsub( data_str, "^.-::", "" )
+
+		m.debug( "Received " .. command )
+
+		local success, data = ace_serializer.Deserialize( M, data_str )
+		if success then
+			if data then
+				data = decode( data )
+			end
+
+			on_command( command, data, sender )
+		else
+			m.error( "Corrupt data in addon message!" )
+		end
+	end
+
+	ace_comm.RegisterComm( M, m.prefix, on_comm_received )
+
+	---@type MessageHandler
+	return {
+		send_tradeskill = send_tradeskill,
+		request_tradeskill = request_tradeskill,
+		request_tradeskills = request_tradeskills,
+		version_check = version_check
+	}
+end
+
+m.MessageHandler = M
+return M
